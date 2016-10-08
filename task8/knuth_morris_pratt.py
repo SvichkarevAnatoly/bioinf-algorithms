3164a3f8330d9ca71d53a4b46c8bd454013756f9
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-10-09 00:37:42 +0300

    done task 8 and test

--- task8/knuth_morris_pratt.py
+++ task8/knuth_morris_pratt.py
@@ -1,5 +1,28 @@
+def prefix(s):
+    v = [0] * len(s)
+    for i in range(1, len(s)):
+        k = v[i - 1]
+        while k > 0 and s[k] != s[i]:
+            k = v[k - 1]
+        if s[k] == s[i]:
+            k += 1
+        v[i] = k
+    return v
+
+
 def search(s, sub):
-    pass
+    index = -1
+    f = prefix(sub)
+    k = 0
+    for i in range(len(s)):
+        while k > 0 and sub[k] != s[i]:
+            k = f[k - 1]
+        if sub[k] == s[i]:
+            k += 1
+        if k == len(sub):
+            index = i - len(sub) + 1
+            break
+    return index
 
 
 def main():
--- task8/test8.py
+++ task8/test8.py
@@ -3,6 +3,17 @@ import knuth_morris_pratt as kmp
 
 
 class Test(unittest.TestCase):
+    def test_prefix(self):
+        s = "abcdabcabcdabcdab"
+        self.assertEqual(
+            [0, 0, 0, 0, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 4, 5, 6],
+            kmp.prefix(s))
+
+    def test_middle_prefix(self):
+        s = "abcabgabcabc"
+        sub = "abcabc"
+        self.assertEqual(6, kmp.search(s, sub))
+
     def test_found_at_start(self):
         s = "abracadabra"
         sub = "abr"
